ac664bd4aa61c3c4d4ee25fd8f0285f217cebb69
tebogomonamodi08 <150713484+tebogomonamodi08@users.noreply.github.com>
2025-04-01 12:48:09 +0200

    News API test project successful run

---
+++ requests(Exercises)/news_api.py
@@ -0,0 +1,22 @@
+import requests
+
+url = 'https://newsapi.org/v2/top-headlines'
+params = {
+    'apiKey': '6a9fdf0c6b9e4f41aa32c92c9aec5a42',
+    'category': 'general',
+    'language':'en',
+    'country':'us'
+}
+
+try:
+    response = requests.get(url, timeout=15, params=params)
+    response.raise_for_status()
+except requests.exceptions.HTTPError as e:
+    print(e)
+except requests.ConnectionError as e:
+    print(e)
+except requests.Timeout as e:
+    print(e)
+except requests.JSONDecodeError as e:
+    print(e)
+print(response.text)
